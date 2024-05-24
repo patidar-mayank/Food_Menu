403020141df4412076145a323eee008bca859a43
patidar-mayank <210305124102@paruluniversity.ac.in>
2024-05-24 08:46:59 +0530

    Initial commit

---
+++ food_item/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name="index"),  #empty space that indicate there is no url after food that write in project urls if you write after project 
+                                       # file then you write in empty place just like "hello/".
+    path('item/',views.item,name="item"),                                   
+]
---
+++ food_item/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
+from django.template import loader
+
+# Create your views here.
+#---this is one way to render template
+# def index(request):
+#     item_list=Item.objects.all()
+#     template=loader.get_template('food\index.html')
+#     context={
+
+#     }
+#     return HttpResponse(template.render(context,request))
+
+#another view ,view is python function
+
+# second way to render template
+
+def index(request):
+    item_list=Item.objects.all()
+    template=loader.get_template('food\index.html')
+    context={
+        'item_list':item_list,
+
+   }
+    return render(request,'food\index.html',context)
+
+def item(request):
+    return HttpResponse('<h1>this is second view</h1>')
