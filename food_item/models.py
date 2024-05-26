daa43c186de94617de235d6f9f265da03af31603
patidar-mayank <210305124102@paruluniversity.ac.in>
2024-05-26 13:55:02 +0530

    Add image

--- food_item/models.py
+++ food_item/models.py
@@ -9,4 +9,5 @@ class Item(models.Model):
     item_name=models.CharField(max_length=200)
     item_desc=models.CharField(max_length=200)
     item_price=models.IntegerField()
+    item_image=models.CharField(max_length=500,default="https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg")
 
