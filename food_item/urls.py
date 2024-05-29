7e827748d52ebe7514688fcf09a7cd4c6abf14ef
patidar-mayank <210305124102@paruluniversity.ac.in>
2024-05-29 14:34:39 +0530

    add and delete image

--- food_item/urls.py
+++ food_item/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+app_name='food'
 
 urlpatterns = [
     path('',views.index,name="index"),  #empty space that indicate there is no url after food that write in project urls if you write after project 
                                        # file then you write in empty place just like "hello/".
     path('item/',views.item,name="item"),  
-    path('<int:item_id>/',views.detail,name="detail"),                          
+    path('<int:item_id>/',views.detail,name="detail"),  
+    path('add',views.create_item,name='create_item'),  
+    path('update/<int:item_id>/',views.update_item,name='update_item'),
+    path('delete/<int:item_id>',views.delete_item,name="delete_item"),                      
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "users.apps.UsersConfig",
     "food_item.apps.FoodItemConfig",
     "django.contrib.admin",
     "django.contrib.auth",
