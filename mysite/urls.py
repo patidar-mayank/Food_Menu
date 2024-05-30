da815a01ac78311a30370bd97a3ec19c8f259c62
patidar-mayank <210305124102@paruluniversity.ac.in>
2024-05-30 19:16:45 +0530

    ADD  Register Page

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from users import views as user_views
+from django.contrib.auth import views as authentications_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('food/',include('food_item.urls')),
     path('register/',user_views.register,name='register'),
+    path('login/',authentications_views.LoginView.as_view(template_name='users/login.html'),name="login"),
+    path('logout/',authentications_views.LogoutView.as_view(template_name='users/logout.html'),name="logout"),
+
 ]
